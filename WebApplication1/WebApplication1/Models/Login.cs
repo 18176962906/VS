de2f769ec584acdac023e63a889fc9bdf5068e44
18176962906 <1761824166@qq.com>
2019-01-11 14:25:39 +0800

    。

---
+++ WebApplication1/WebApplication1/Models/Login.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Login
+    {
+        public string UserName
+        {
+            get;
+            set;
+        }
+        public string Password
+        {
+            get;
+            set;
+        }
+        public override string ToString()
+        {
+            return this.UserName + "-" + this.Password;
+        }
+
+
+    }
+}
